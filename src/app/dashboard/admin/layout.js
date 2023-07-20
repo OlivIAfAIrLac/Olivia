d1036171f64ad46e5656103e247167e65dd2a846
nachomartinez30 <nachomartinez3010@gmail.com>
2023-07-19 19:15:30 -0600

    amdin routes protected

---
+++ src/app/dashboard/admin/layout.js
@@ -0,0 +1,27 @@
+'use client'
+import { routes } from "@/helpers/routes";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function RootLayout({ children }) {
+    const router = useRouter()
+    const { data, status } = useSession()
+
+    const checkRole = (role) => {
+        role !== 'admin' && router.push(routes.dashboard.main)
+    }
+
+    useEffect(() => {
+        status !== 'loading' && checkRole(data.user.rol);
+    }, [status])
+
+
+    return (
+        <div>
+            {(status !== 'loading' && data.user.rol === 'admin') && children}
+        </div>
+
+
+    )
+}
