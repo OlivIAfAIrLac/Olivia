f3d910436a0d01a23e3a594095e62bfb5f8d658a
nachomartinez30 <nachomartinez3010@gmail.com>
2023-07-26 11:37:19 -0600

    upload files on odcument section

--- src/app/dashboard/expediente/[id]/page.jsx
+++ src/app/dashboard/expediente/[id]/page.jsx
@@ -5,6 +5,7 @@ import Container from "@/components/Container";
 import DateTimeDisplayer from "@/components/DateTimeDisplayer";
 import IconButton from "@/components/IconButton";
 import IconFile from "@/components/IconFile";
+import LoaderSkeleton from "@/components/LoaderSkeleton";
 import Modal from "@/components/Modal";
 import { apiRoutes } from "@/helpers/apiRoutes";
 import { routes } from "@/helpers/routes";
@@ -12,7 +13,7 @@ import axios from "axios";
 import Link from "next/link";
 import { useCallback, useEffect, useState } from "react";
 import { BiSolidMicrophone } from "react-icons/bi";
-import { FaFilePdf, FaRegEye } from "react-icons/fa6";
+import { FaRegEye } from "react-icons/fa6";
 import { TbUpload } from "react-icons/tb";
 
 
@@ -23,16 +24,40 @@ const HomeFolio = ({ params }) => {
     const [openModalAudio, setOpenModalAudio] = useState(false)
     const [openModalDocument, setOpenModalDocument] = useState(false)
     const [selectedFiles, setSelectedFiles] = useState([])
+    const [refresh, setRefresh] = useState(false)
 
-    const handleUploadAudio = (ev) => {
+    const handleOpenModalAudio = (ev) => {
         setOpenModalAudio(true)
     }
-    const handleUploadDocument = (ev) => {
+    const handleOpenModalDocument = (v) => {
+        setSelectedFiles([])
         setOpenModalDocument(true)
     }
     const handleDocumentOnChange = (ev) => {
         ev.preventDefault()
-        setSelectedFiles([...selectedFiles, ev.target.files])
+        setSelectedFiles(ev.target.files)
+    }
+    const handleSubmit = async (ev) => {
+        ev.preventDefault()
+        let formData = new FormData()
+        try {
+            for (let i = 0; i < selectedFiles.length; i++) {
+                formData.append("documentos", selectedFiles[i]);
+            }
+            formData.append("expediente", id)
+            const res = await axios.post(apiRoutes.DOCUMENTO, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            if (res.status === 200) {
+                setOpenModalDocument(false)
+                setRefresh(true)
+                setSelectedFiles([])
+            }
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     const getData = useCallback(async () => {
@@ -40,12 +65,14 @@ const HomeFolio = ({ params }) => {
             const res = await axios.get(`${apiRoutes.EXPEDIENTE}/${id}`);
             if (res.status === 200) {
                 setExpedienteData({ ...res.data })
+                setRefresh(false)
             }
         } catch (error) {
             /* TODO: Handle error messages */
             console.error(error);
         }
-    }, [id]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [id, refresh]);
 
     const AudioModal = () => <Modal
         open={openModalAudio}
@@ -61,32 +88,44 @@ const HomeFolio = ({ params }) => {
         setOpen={setOpenModalDocument}
     >
         {/* TODO: Complete documents upload */}
-        <div className="mt-8 login-bg p-3 capitalize">
-            arrastra aqui tus documentos
-            <div className="p-3 grid grid-flow-col">
-                {selectedFiles.map((file, index) => {
-                    const extension = file[0].name.split('.')[1]
-                    return <IconFile
-                        extension={extension}
-                        size={35}
-                        key={`file_${index}`}
-                    />
-                })}
+        <div className="mt-8 login-bg p-3">
+            <span>Arrastra aqui tus documentos</span>
+            <div className="p-3 grid grid-flow-row grid-cols-4 gap-3 ">
+                {
+                    Array.from(selectedFiles).map(
+                        (file, index) => {
+                            const extension = file.name.split('.')[1]
+                            return <IconFile
+                                extension={extension}
+                                size={35}
+                                key={`file_${index}`}
+                            />
+                        })
+                }
             </div>
 
         </div>
-        <div className="flex flex-row mt-2">
-            <label htmlFor="documento" className="ml-auto">Buscar en mi documentos</label>
-            <input id="documento" type="file" className="hidden" onChange={handleDocumentOnChange} />
-        </div>
-        {/* Submit button */}
-        <div className="grid grid-flow-col gap-6 text-center">
-            <button className="font-bold save-bg-btn capitalize py-3 w-full mt-5"
-                onClick={() => setOpen(false)}
-            >
-                Guardar
-            </button>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div className="flex flex-row mt-2">
+                <label htmlFor="documento" className="ml-auto">Buscar en mi documentos</label>
+                <input
+                    id="documento"
+                    type="file"
+                    multiple
+                    accept=".jpg,.jpeg,.png,.doc,.docx,.pdf"
+                    className="hidden"
+                    onChange={handleDocumentOnChange}
+                />
+            </div>
+            {/* Submit button */}
+            <div className="grid grid-flow-col gap-6 text-center">
+                <button className="font-bold save-bg-btn capitalize py-3 w-full mt-5"
+                    type="submit"
+                >
+                    Guardar
+                </button>
+            </div>
+        </form>
     </Modal>
 
     useEffect(() => {
@@ -99,63 +138,65 @@ const HomeFolio = ({ params }) => {
             <AudioRecorder />
             <AudioModal />
             <DocumentModal />
-            {expedienteData && <Container>
-                <span className="text-lg mb-4">Expediente</span>
-                <div className="p-6 primary-bg flex flex-col">
-                    <DateTimeDisplayer
-                        timeStamp={expedienteData.expediente.createdAt}
-                    />
-                    <span className="font-bold">Folio {expedienteData.expediente.folio}</span>
-                    <span className="mb-5 capitalize">{expedienteData.expediente.nombre}</span>
-                    {/* Audio Container */}
-                    <div className="p-3 login-bg flex flex-col">
-                        <span className="font-bold">
-                            Audio
-                        </span>
-                        <div className="px-5 mt-2 flex flex-col ">
-                            <div className="border-b-2 border-color-primary  flex flex-row">
-                                {/* Audio descripcion */}
-                                <span className="pl-5 mt-4">
-                                    {/* {expedienteData.audio[0]?.descripcion} */}
-                                </span>
-                                <ControlButtonsGroup
-                                    onView={() => true}
-                                    onRemove={() => true}
-                                />
+            {!expedienteData ? <LoaderSkeleton />
+                : <Container>
+                    <span className="text-lg mb-4">Expediente</span>
+                    <div className="p-6 primary-bg flex flex-col">
+                        <DateTimeDisplayer
+                            timeStamp={expedienteData.expediente.createdAt}
+                        />
+                        <span className="font-bold">Folio {expedienteData.expediente.folio}</span>
+                        <span className="mb-5 capitalize">{expedienteData.expediente.nombre}</span>
+                        {/* Audio Container */}
+                        <div className="p-3 login-bg flex flex-col">
+                            <span className="font-bold">
+                                Audio
+                            </span>
+                            <div className="px-5 mt-2 flex flex-col ">
+                                <div className="border-b-2 border-color-primary  flex flex-row">
+                                    {/* Audio descripcion */}
+                                    <span className="pl-5 mt-4">
+                                        {/* {expedienteData.audio[0]?.descripcion} */}
+                                    </span>
+                                    <ControlButtonsGroup
+                                        onView={() => true}
+                                        onRemove={() => true}
+                                    />
+                                </div>
                             </div>
+                            {/* Buttons */}
+                            <RecordsButtonGroup
+                                handleUploadAudio={handleOpenModalAudio}
+                            />
                         </div>
-                        {/* Buttons */}
-                        <RecordsButtonGroup
-                            handleUploadAudio={handleUploadAudio}
+                        {/* Documentos Container */}
+                        <div className="mt-2 p-3 login-bg flex flex-col">
+                            <span className="font-bold">Documentos</span>
+                            {
+                                expedienteData.documentos.map((item, index) => <DocumentViewer
+                                    key={item.descripcion + index}
+                                    descripcion={item.descripcion}
+                                />)
+                            }
+                            {/* Upload doc button */}
+                            <UploadDocumentButton
+                                onClick={handleOpenModalDocument}
+                            />
+                        </div>
+                        {/* Buttons group */}
+                        <ButtonGroup
+                            folio={id}
                         />
                     </div>
-                    {/* Documentos Container */}
-                    <div className="mt-2 p-3 login-bg flex flex-col">
-                        <span className="font-bold">Documentos</span>
-                        {
-                            expedienteData.documentos.map((item, index) => <DocumentViewer
-                                key={item.descripcion + index}
-                                descripcion={item.descripcion}
-                            />)
-                        }
-                        {/* Upload doc button */}
-                        <UploadDocumentButton
-                            onClick={handleUploadDocument}
-                        />
+                    <div className="flex flex-col text-center justify-center items-center">
+                        <Link className="font-bold primary-bg capitalize py-3 px-12 mt-5"
+                            href={routes.dashboard.main}
+                        >
+                            más expedientes
+                        </Link>
                     </div>
-                    {/* Buttons group */}
-                    <ButtonGroup
-                        folio={id}
-                    />
-                </div>
-                <div className="flex flex-col text-center justify-center items-center">
-                    <Link className="font-bold primary-bg capitalize py-3 px-12 mt-5"
-                        href={routes.dashboard.main}
-                    >
-                        más expedientes
-                    </Link>
-                </div>
-            </Container>}
+                </Container>
+            }
         </div>
     );
 }
