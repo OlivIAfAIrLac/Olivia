3b313ee9f748edc49b6cf3ab63af9fafb9843dfa
nachomartinez30 <nachomartinez3010@gmail.com>
2023-08-09 11:56:27 -0600

    hotfix

--- src/components/dynamic-components/Textarea.jsx
+++ src/components/dynamic-components/Textarea.jsx
@@ -1,6 +1,7 @@
 export default function Textarea(props) {
     return (
         <textarea className="border border-solid border-gray-600 p-1 my-1 w-full"
+            defaultValue={props.defaultValue}
             onBlur={() => props.onBlur()}
             onChange={(e) => { props.onChange(e, props.block._uid) }}
         />
