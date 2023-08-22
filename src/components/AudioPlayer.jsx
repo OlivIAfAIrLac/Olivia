8deb6ca54242d17a8a8d2367d15f77c1aea3ca8f
nachomartinez30 <nachomartinez3010@gmail.com>
2023-08-22 13:26:03 -0600

    m3latam mimetype

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -7,7 +7,7 @@ const AudioPlayer = ({
             <source src={src} type="audio/x-wav" />
             <source src={src} type="audio/mpeg" />
             <source src={src} type="audio/mp3" />
-            <source src={src} type="audio/mp4a" />
+            <source src={src} type="audio/mp4a-latm" />
             <source src={src} type="audio/m4a" />
             Tu navegador no soporta reproductor de audio
         </audio>
