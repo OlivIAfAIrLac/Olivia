cb48d2f96e0597012e7a3614d32d89e11331c391
nachomartinez30 <nachomartinez3010@gmail.com>
2023-08-22 12:32:38 -0600

    src changed

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -2,12 +2,12 @@ const AudioPlayer = ({
     src
 }) => {
     return (
-        <audio controls autoPlay className="primary-bg">
-            <source src={src} />
+        <audio controls autoPlay className="primary-bg" src={src} >
+            {/* <source src={src} />
             <source src={src} type="audio/x-wav" />
             <source src={src} type="audio/mpeg" />
             <source src={src} type="audio/m4a" />
-            Tu navegador no soporta reproductor de audio
+            Tu navegador no soporta reproductor de audio */}
         </audio>
     );
 }
